0d7c60295b9232c3547e181bc4d44f16a1ad494f
svikaasni <svikaasni@gmail.com>
2026-07-04 16:20:06 +0530

    Add files via upload

---
+++ StudentInfoSystem/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String department;
+
+    public Student(int id, String name, int age, String department) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getDepartment() { return department; }
+
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+    public void setDepartment(String department) { this.department = department; }
+}
